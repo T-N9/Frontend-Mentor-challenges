368bbe8809566c01fbe3ec428e84f2105c9a5d68
T-N9 <tenyainmoelwin@gmail.com>
2021-07-20 10:36:02 +0630

    [tn] animated classes updated

--- digital-banking/root/resources/js/app.js
+++ digital-banking/root/resources/js/app.js
@@ -1,4 +1,15 @@
 
-document.querySelector('.header').onclick=function(){
-    this.classList.toggle('header-open');
+const getHeader_menu_btn=document.querySelector('.header__menu');
+const getOverlay_menu=document.querySelector('.menu-overlay');
+const getNav_sm=document.querySelector('.sm-nav-wrapper');
+
+
+getHeader_menu_btn.onclick=function(){
+    this.classList.toggle('menu-btn-open');
+    getOverlay_menu.classList.toggle('fade-out');
+    getOverlay_menu.classList.toggle('fade-in');
+
+    getNav_sm.classList.toggle('fade-out-up');
+    getNav_sm.classList.toggle('fade-in-down');
 }
+
